3a02ffd6635e25c6c791518fc51f3aadbcb32c26
shreeram14105 <133945803+shreeram14105@users.noreply.github.com>
2025-10-21 19:28:27 +0530

    Initialize Flask app with welcome route

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to Wisecow 🐮</h1><p>Containerized and deployed on Kubernetes with TLS!</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
